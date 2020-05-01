a73e3314941d9cad20240cf05b111855b1c41ba2
Ian Ornelas <ian.orn@gmail.com>
2020-04-30 21:02:31 -0300

    Updates

---
+++ precache-manifest.7edf747e7df29c4209a69b2fe2165cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e9e51c8089ab1ca3eaa54669c4036f",
+    "url": "/ascension-calculator/index.html"
+  },
+  {
+    "revision": "1e25e4ae8562d6968bfc",
+    "url": "/ascension-calculator/static/css/main.fce0872a.chunk.css"
+  },
+  {
+    "revision": "ecdf19b1afa8775a75cd",
+    "url": "/ascension-calculator/static/js/2.1b4f3547.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ascension-calculator/static/js/2.1b4f3547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e25e4ae8562d6968bfc",
+    "url": "/ascension-calculator/static/js/main.27aaff16.chunk.js"
+  },
+  {
+    "revision": "18b0a05991369703029f",
+    "url": "/ascension-calculator/static/js/runtime-main.f466fe3f.js"
+  }
+]);
